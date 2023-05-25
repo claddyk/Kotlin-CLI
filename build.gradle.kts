5ac27a7ff4775b450e60483f9998834bf8ae0986
Mohit Kumar <madscientists1523@gmail.com>
2023-05-25 21:08:47 +0530

    Updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,9 +22,6 @@ dependencies {
     implementation("io.ktor:ktor-client-serialization:2.3.0")
     implementation("io.ktor:ktor-client-content-negotiation:2.3.0")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.0")
-
-
-
 }
 
 tasks.test {
